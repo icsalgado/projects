35be490b8ad8ed9302f8e74f72538fa36a2a9b87
icsalgado <icsalgado@icloud.com>
2024-01-08 00:31:25 -0300

    preparando para listar

--- market_list/index.php
+++ market_list/index.php
@@ -6,9 +6,17 @@
     <title>Document</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
 </head>
 <body>
+
+    <?php //esse array vai ser substituido pelos dados do banco
+        $items = array(
+            array('id' => 1, 'Marca' => 'Dodge', 'Modelo' => 'Hemi Challenger', 'Ano' => 1970, 'Cor' => 'Laranja'),
+            array('id' => 2, 'Marca' => 'Mitsubishi', 'Modelo' => 'Eclipse Spider', 'Ano' => 2004, 'Cor' => 'Cinza'),
+            array('id' => 3, 'Marca' => 'Buick ', 'Modelo' => 'Riviera', 'Ano' => 1964, 'Cor' => 'Preto')
+        );
+    ?>
     <table class="table table-bordered table-dark">
     <thead>
         <tr>
